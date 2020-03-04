db96ecf79a4d0447a18675e916ba1b9858c7ce5f
Daniel Moniz <daniel.moniz@gmail.com>
2020-03-03 22:00:11 -0500

    Add docker setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Installations -----------
+sudo apt-get update
+
 # git
 sudo apt install git
 git config --global user.email "daniel.moniz@gmail.com"
@@ -46,3 +48,17 @@ source ~/.bashrc
 pyenv update
 
 pip install virtualenv
+
+# docker
+sudo apt-get remove docker docker-engine docker.io containerd runc # uninstall old versions
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88 # verify fingerprint
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+sudo groupadd docker # create docker user group
+sudo usermod -aG docker $USER # add current user to docker group
+sudo docker run hello-world # check that docker is now working
+# NOTE: Need to log out and log back in again to use Docker without sudo
+
